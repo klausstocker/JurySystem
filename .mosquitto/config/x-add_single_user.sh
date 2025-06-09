77193330c6441010aac2b8c860ae9f2bb7fe2b98
fkrenn12 <fkrenn@nanet.at>
2025-06-09 18:57:10 +0200

    fix chmod

--- .mosquitto/config/x-add_single_user.sh
+++ .mosquitto/config/x-add_single_user.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-sh /mosquitto/config/set_permissions.sh
 echo -n "Enter Username: "
 read Username
-docker exec -it mosquitto sh -c '/mosquitto/config/set_permissions.sh'
 docker exec -it mosquitto mosquitto_passwd /mosquitto/config/passwd $Username
 docker exec -it mosquitto sh -c 'kill -HUP $(cat /mosquitto/config/pid)'
 # mosquitto_passwd /mosquitto/config/passwd $Username
--- .mosquitto/config/x-reload_user_password_configuration.sh
+++ .mosquitto/config/x-reload_user_password_configuration.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-# sh /mosquitto/config/set_permissions.sh
-# kill -HUP $(cat /mosquitto/config/pid)
-docker exec -it mosquitto sh -c '/mosquitto/config/set_permissions.sh'
 docker exec -it mosquitto sh -c 'kill -HUP $(cat /mosquitto/config/pid)'
